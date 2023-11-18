de5d9122f2b547dd1a59ea4a33423191409623a4
Oichkatzelesfrettschen <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2023-11-17 22:11:04 -0800

    Update kernameters.py

--- kernameters.py
+++ kernameters.py
@@ -1,158 +1,139 @@
 import subprocess
 from reportlab.lib.pagesizes import landscape, letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 
-
-# Function to run a command in the shell and return its output.
-def run_command(command):
-    # Create a subprocess to run the shell command.
+def run_command(command: str) -> str:
+    """
+    Run a shell command and return the output as a string.
+    """
     process = subprocess.Popen(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    (
-        stdout,
-        stderr,
-    ) = process.communicate()  # Capture the output and error (if any) of the command.
-    if process.returncode != 0:
-        # If the command failed (return code not 0), return None.
-        return None
-    return stdout.decode("utf-8")  # Decode the output from bytes to string and return.
+    stdout, stderr = process.communicate()
+    return None if process.returncode != 0 else stdout.decode("utf-8")
 
-
-# Function to get module parameters from the 'modinfo' command.
-def get_modinfo_params(module):
-    output = run_command(
-        f"modinfo -p {module}"
-    )  # Run 'modinfo -p' for the given module.
-    if output is None or output.strip() == "":
-        # If there is no output or it's empty, return None.
-        return None
+def get_modinfo_params(module: str):
+    """
+    Retrieve modinfo parameters for a given module.
+    """
+    if not isinstance(module, str) or not module:
+        return None  # Handle non-string or empty module names
+    output = run_command(f"modinfo -p {module}")
+    if not output:
+        return None  # Handle module not found in modinfo
     params = {}
     for line in output.strip().split("\n"):
-        # Split each line by ':' to separate the parameter name and its description.
         parts = line.split(":")
         if len(parts) >= 2:
-            # Add the parameter and its description to the dictionary.
             params[parts[0].strip()] = parts[1].strip()
     return params
 
-
-# Function to get current module parameters from 'systool'.
-def get_systool_params(module):
-    output = run_command(
-        f"systool -v -m {module}"
-    )  # Run 'systool -v -m' for the given module.
-    if output is None:
-        # If there is no output, return an empty dictionary.
-        return {}
+def get_systool_params(module: str):
+    """
+    Retrieve systool parameters for a given module.
+    """
+    if not isinstance(module, str) or not module:
+        return {}  # Handle non-string or empty module names
+    output = run_command(f"systool -v -m {module}")
+    if not output:
+        return {}  # Handle module not found in systool
     params = {}
-    reading_params = False  # Flag to indicate if we are reading the parameters section.
+    reading_params = False
     for line in output.strip().split("\n"):
         if reading_params:
-            # Process the lines when we are in the parameters section.
-            if line.strip() == "":
-                # If we reach an empty line, end of parameters section.
+            if line.strip() == '':
                 break
-            parts = line.split("=")
+            parts = line.split('=')
             if len(parts) == 2:
-                # Add the parameter and its current value to the dictionary.
                 params[parts[0].strip()] = parts[1].strip()
-        elif "Parameters:" in line:
-            # Start reading parameters when we find the 'Parameters:' line.
+        elif 'Parameters:' in line:
             reading_params = True
     return params
 
+def create_paragraph(text: str, styles):
+    """
+    Create a formatted paragraph for long text.
+    """
+    if not text:
+        return Paragraph("", styles["Normal"])  # Handle empty text input
+    return Paragraph(text, styles["Normal"])
 
-# Function to create a formatted paragraph for long text.
-def create_paragraph(text):
-    styles = getSampleStyleSheet()  # Get default styles for paragraphs.
-    return Paragraph(text, styles["Normal"])  # Create a paragraph with the given text.
+def create_table_data(module_data: dict):
+    """
+    Create table data for modules with loaded and available parameters.
+    """
+    if not module_data:
+        return [["Module", "Loaded Parameters", "Available Parameters"]]  # Handle empty module_data input
+    table_data = [["Module", "Loaded Parameters", "Available Parameters"]]
+    for module, data in module_data.items():
+        if data is None:
+            table_data.append([module, "No data available", ""])
+        else:
+            first = True
+            for param, values in data.items():
+                loaded_para = create_paragraph(f'{param}: {values["current"]}', style)
+                available_para = create_paragraph(f'{param}: {values["available"]}', style)
+                if first:
+                    table_data.append([module, loaded_para, available_para])
+                    first = False
+                else:
+                    table_data.append(["", loaded_para, available_para])
+    return table_data
 
-
-# Function to create the PDF report.
-def create_pdf_report(module_data, filename):
-    doc = SimpleDocTemplate(
-        filename, pagesize=landscape(letter)
-    )  # Create a PDF document in landscape orientation.
-    elements = []
-
-    # Sorting modules into those with data and those without.
-    modules_with_data = sorted([m for m, d in module_data.items() if d is not None])
-    modules_no_data = sorted([m for m, d in module_data.items() if d is None])
-
-    for module_list in [modules_with_data, modules_no_data]:
-        for module in module_list:
-            data = module_data[module]
-            table_data = [["Module", "Loaded Parameters", "Available Parameters"]]
-            if data is None:
-                # For modules with no data, add a row indicating this.
-                table_data.append([module, "No data available", ""])
-            else:
-                first = True  # Flag to handle the first parameter differently.
-                for param, values in data.items():
-                    # Create paragraphs for loaded and available parameter values.
-                    loaded_para = create_paragraph(f'{param}: {values["current"]}')
-                    available_para = create_paragraph(f'{param}: {values["available"]}')
-                    if first:
-                        # For the first parameter, include the module name in the first cell.
-                        table_data.append([module, loaded_para, available_para])
-                        first = False
-                    else:
-                        # For subsequent parameters, leave the first cell empty.
-                        table_data.append(["", loaded_para, available_para])
-            # Create a table with the data and set its style.
-            t = Table(table_data, colWidths=[100, 300, 300], repeatRows=1)
-            t.setStyle(
-                TableStyle(
-                    [
-                        ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-                        ("BACKGROUND", (0, 1), (-1, -1), colors.whitesmoke),
-                        ("TEXTCOLOR", (0, 0), (-1, 0), colors.black),
-                        ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-                        ("FONTSIZE", (0, 0), (-1, -1), 8),
-                        ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                    ]
-                )
+def create_pdf_report(module_data: dict, filename: str):
+    """
+    Create a PDF report with module data.
+    """
+    if not filename:
+        print("Error: Filename for PDF report is empty.")
+        return
+    try:
+        doc = SimpleDocTemplate(filename, pagesize=landscape(letter))
+        style = getSampleStyleSheet()
+        table_data = create_table_data(module_data)
+        elements = [Table(table_data, colWidths=[100, 300, 300], repeatRows=1).setStyle(
+            TableStyle(
+                [
+                    ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+                    ("BACKGROUND", (0, 1), (-1, -1), colors.whitesmoke),
+                    ("TEXTCOLOR", (0, 0), (-1, 0), colors.black),
+                    ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                    ("FONTSIZE", (0, 0), (-1, -1), 8),
+                    ("VALIGN", (0, 0), (-1, -1), "TOP"),
+                ]
             )
-            elements.append(t)  # Add the table to the document elements.
+        )]
+        doc.build(elements)
+    except Exception as e:
+        print(f"Error in creating PDF report: {e}")
 
-    doc.build(elements)  # Build the PDF document with the accumulated elements.
-
-
-# Main function to run the script.
 def main():
-    modules = run_command(
-        "lsmod | awk 'NR>1 {print $1}'"
-    )  # Get the list of loaded kernel modules.
-    if modules is None:
-        print("Error: Unable to retrieve kernel modules.")
+    """
+    Main function to run the script.
+    """
+    modules = run_command("lsmod | awk 'NR>1 {print $1}'")
+    if not modules:
+        print("Error: Unable to retrieve kernel modules or no modules loaded.")
         return
 
     module_data = {}
     for module in set(modules.strip().split("\n")):
-        modinfo_params = get_modinfo_params(module)  # Get available module parameters.
-        systool_params = get_systool_params(module)  # Get current module parameters.
+        if not module:  # Skip empty module names
+            continue
+        modinfo_params = get_modinfo_params(module)
+        systool_params = get_systool_params(module)
         if modinfo_params is None:
-            # If no data available from modinfo, mark as None.
             module_data[module] = None
         else:
-            # Compare current and available parameters.
-            comparison = {
-                param: {
-                    "current": systool_params.get(param, "N/A"),
-                    "available": modinfo_params.get(param, "N/A"),
-                }
-                for param in set(modinfo_params) | set(systool_params)
-            }
+            comparison = {param: {"current": systool_params.get(param, "N/A"),
+                                  "available": modinfo_params.get(param, "N/A")}
+                          for param in set(modinfo_params) | set(systool_params)}
             module_data[module] = comparison
 
-    create_pdf_report(
-        module_data, "module_parameters_report.pdf"
-    )  # Create the PDF report.
+    create_pdf_report(module_data, "module_parameters_report.pdf")
     print("Report generated: module_parameters_report.pdf")
-
 
 if __name__ == "__main__":
     main()
